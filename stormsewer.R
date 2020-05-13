aad9cc81639a7ea29c8113ea05c16a97a40e8712
LinneaRock <lrock@wisc.edu>
2020-05-13 14:26:12 -0500

    organizing

--- stormsewer.R
+++ stormsewer.R
@@ -1,47 +1,35 @@
 library(tidyverse)
+source("Functions/cond.R")
+source("Functions/splot.R")
+source("Functions/clseries.R")
 
-#Function to plot time series of specific conductance 
-cond <- function(df, X) {
-  ggplot(df, aes(date, sp.cond)) +
-    geom_line() +
-    labs(y = "Specific Conductivity (µS/cm) @ 25°C\n", 
-         x = "\nDate") +
-    theme(panel.background = element_rect(fill = "white", colour = "white",
-                                          size = 2, linetype = "solid"),
-          panel.grid.major = element_line(size = 0.25, linetype = 'solid',
-                                          colour = "gray88"), 
-          panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                          colour = "gray88"),
-          axis.text = element_text(size =13, face = "bold"),
-          axis.title = element_text(size =13, face = "bold"))
-  
-  ggsave(filename = paste("Plots/conductance_time_series/", X, ".png", sep = ""))
-}
+#Willow Creek conductivity time series
 
-cond(upstream, "WC - Upstream")
-cond(downstream, "WC - Downstream")
+cond(upstream)
+splot("conductance_time_series/", "WC - Upstream")
+
+cond(downstream)
+splot("conductance_time_series/", "WC - Downstream")
 
 
-#Function to plot time series of chloride concentrations
-cl <- function(df, X) {
-  ggplot(df, aes(date, chloride_mgL)) +
-    geom_line() +
-    geom_smooth(method = "lm", se = FALSE)
-    labs(y = "Chloride Concentration (mg/L)\n", 
-         x = "\nDate") +
-    theme(panel.background = element_rect(fill = "white", colour = "white",
-                                          size = 2, linetype = "solid"),
-          panel.grid.major = element_line(size = 0.25, linetype = 'solid',
-                                          colour = "gray88"), 
-          panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                          colour = "gray88"),
-          axis.text = element_text(size =13, face = "bold"),
-          axis.title = element_text(size =13, face = "bold"))
-  
-  ggsave(filename = paste("Plots/chloride_time_series/", X, ".png", sep = ""))
-}
+#chloride concentrations of Spring Harbor from 2014-2016
 
-cl(labSH, "SH")
+clseries(labSH) 
+splot("chloride_time_series/", "SH")
 
+ggplot(labSH, aes(date, chloride_mgL)) +
+  geom_line() +
+  labs(y = "Chloride Concentration (mg/L)\n",
+       x = "") +
+  theme(panel.background = element_rect(fill = "white", colour = "white",
+                                        size = 2, linetype = "solid"),
+        panel.grid.major = element_line(size = 0.25, linetype = 'solid',
+                                        colour = "gray88"), 
+        panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                        colour = "gray88"),
+        axis.text = element_text(size = 11),
+        axis.title = element_text(size = 11))
+
+splot("chloride_time_series/", "SH_alt")
 
 
