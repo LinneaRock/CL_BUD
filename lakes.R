d85dbbc0b4f3de496842b591bf3a867f0b697a9c
LinneaRock <lrock@wisc.edu>
2020-06-01 16:44:21 -0500

    trying a moving average for Mendota conductivity data

--- lakes.R
+++ lakes.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(lubridate)
 library(ggpubr)
+library(zoo)
 source("Functions/cond.R")
 source("Functions/splot.R")
 
@@ -13,6 +14,8 @@ splot("conductance_time_series/", "ME")
 lakecond(loggerMO_Epi, loggerMO_Hypo, "Monona 20m", "Monona 1.5m")
 splot("conductance_time_series/", "MO")
 
+
+#checking max and min values for the Mendota data
 checkepi <- loggerME_Epi %>%
   mutate(day = as.Date(date, "%m/%d/%y", tz = "America/Chicago")) %>%
   group_by(day) %>%
@@ -28,3 +31,31 @@ checkhypo <- loggerME_Hypo %>%
 ggplot() +
   geom_line(checkepi, mapping = aes(day, MIN)) +
   geom_line(checkhypo, mapping = aes(day, MIN))
+
+
+#Trying a moving daily average to see if it helps with the diurnal fluctuations in the part of the data taht has a lot of fluctuation
+
+RMepiME <- loggerME_Epi %>%
+  mutate(rollingmean = rollmean(sp.cond, 47, fill = NA))
+
+ggplot(RMepiME) +
+  geom_line(aes(date, sp.cond, color = "original")) +
+  geom_line(aes(date, rollingmean, color = "zoo"))
+
+RMhypoME <- loggerME_Hypo %>%
+  mutate(rollingmean = rollmean(sp.cond, 47, fill = NA))
+
+ggplot(RMhypoME) +
+  geom_line(aes(date, sp.cond, color = "original")) +
+  geom_line(aes(date, rollingmean, color = "zoo"))
+
+
+RMepiME <- RMepiME %>%
+  select(date, rollingmean) %>%
+  rename(sp.cond = rollingmean)
+
+RMhypoME <- RMhypoME %>%
+  select(date, rollingmean) %>%
+  rename(sp.cond = rollingmean)
+
+lakecond(RMepiME, RMhypoME, "Mendota 24m", "Mendota 1.5m")
