d36d880a9e3c112b6bf6c28186f0cb5f853c0789
LinneaRock <lrock@wisc.edu>
2020-06-18 10:27:32 -0500

    cleaning up filee and fixing level code

--- Level_Logger_YS.R
+++ Level_Logger_YS.R
@@ -10,32 +10,32 @@ level.data <- read_rds("Data/HOBO_Loggers/YS/Feb3_Mar16/level_data.rds")
 
 
 ###Water Density [kg m^-3]; x = temperature in °C
-watden <- function(x){
-  pt1 <- x + 288.9414
-  pt2 <- 508929 * (x + 68.129630)
-  pt3 <- (x - 3.9863) ^2
+waterDensity <- function(temp){
+  pt1 <- temp + 288.9414
+  pt2 <- 508929 * (temp + 68.129630)
+  pt3 <- (temp - 3.9863) ^2
   1000 * (1 - (pt1/pt2) * pt3)
 }
 
 #density is converted to [lb ft^-3] via:
-densft <- function(d) {
-  d * .0624279606
+densityFT <- function(waterDensity) {
+  waterDensity * 0.0624279606
 }
 
 #Array of downwell pressure values are converted to a density dependent fluid depth array [m] via: 
 #D= FEET_TO_METERS * (KPA_TO_PSI * PSI_TO_PSF * P) / ρ   
 #P = measured pressure, rho = calculated density in [lb ft^-3]
 #The same equation is used to calculate the Barometric Depth, using Barometric pressure rather than measured absoulte pressure
-D <- function(P, rho) {
-  .3048 * (.1450377 * P)/rho
+fluidDepth <- function(Pressure, denistyFT) {
+  0.3048 * (0.1450377 * 144 * Pressure)/denistyFT
 }
 
 #2) Add columns for density and depths
 level.data <- level.data %>%
-  mutate(Density_kgm3 = watden(Temp)) %>%
-  mutate(Density_lbft3 = densft(Density_kgm3)) %>%
-  mutate(Density_Dependent_Depth = D(Pressure, Density_lbft3)) %>%
-  mutate(Barometric_Depth = D(Bar.Pressure, Density_lbft3))
+  mutate(Density_kgm3 = waterDensity(Temp)) %>%
+  mutate(Density_lbft3 = densityFT(Density_kgm3)) %>%
+  mutate(Density_Dependent_Depth = fluidDepth(Pressure, Density_lbft3)) %>%
+  mutate(Barometric_Depth = fluidDepth(Bar.Pressure, Density_lbft3))
 
 
 #3) Calculate water depth
@@ -44,22 +44,27 @@ level.data <- level.data %>%
   #c) calculate the water depth by adding the depth to the bottom of the lake
 
 #The barometric pressure at the reference time is converted to a barometric “depth” 
-D.bar0 <- level.data$Barometric_Depth[1]
+barometricDepth_Mar3 <- level.data$Barometric_Depth[1]
 #The Density dependent depth at reference time
-D.ref <- level.data$Density_Dependent_Depth[1] 
+referenceDepth_Mar3 <- level.data$Density_Dependent_Depth[1] 
 
 #compensation constant is determined using the measured length (.085m) at the time of deployment and the barometric depth at the reference time
-k <- .085 - (D.ref - D.bar0)
+k_Mar3 <- .085 - (referenceDepth_Mar3 - barometricDepth_Mar3)
 
 #Water level from reference point is determined using the following equation:
-L <- function(D, BD) {
-  D - BD + k
+waterLevel <- function(densityDepth, barometricDepth) {
+  densityDepth - barometricDepth + k_Mar3
 }
 
 level.data <- level.data %>%
   mutate(Lreal = L(Density_Dependent_Depth, Barometric_Depth)) %>%
   mutate(Water_Depth = Lreal + .4225) #Add the depth from substrate to the reference depth for the total water depth at the sampling location in meters
- 
+
+
+ggplot(level.data) +
+  geom_line(aes(Date, Water_Depth)) 
+ggplot(level.data) +
+  geom_line(aes(Date, -(Bar.Pressure - Pressure)))
 
 
 
