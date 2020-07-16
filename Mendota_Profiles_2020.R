bbe4a06723563586d7c7d7bad73b6b94353fb54e
LinneaRock <lrock@wisc.edu>
2020-07-16 10:10:54 -0500

    Mendota conductivity profiles

--- Mendota_Profiles_2020.R
+++ Mendota_Profiles_2020.R
@@ -2,24 +2,28 @@ library(tidyverse)
 library(anytime)
 library(viridisLite)
 source("Functions/splot.R")
+source("Functions/L_theme.R")
    
 #read in the csv file rbind them and save as .rds in the git project.
-#attach <- read.csv("C:/Users/Linne/OneDrive/Documents/ME_profile_062520.csv") 
-
-#attach2 <- rbind(ME_profile, attach)%>% #be sure that the previous version of ME_profiles is loaded
+#attach <- read.csv("C:/Users/Linne/OneDrive/Documents/ME_profile_070220.csv") %>%
 #  mutate(sampledate = anytime::anydate(sampledate)) #works better than lubridate in this circumstance
+
+
+#ME_profile <- read_rds("Data/ME_YSI_2020/ME_profiles.rds") 
+
+#attach2 <- rbind(ME_profile, attach) #be sure that the previous version of ME_profiles is loaded
 
 #write_rds(attach2, "Data/ME_YSI_2020/ME_profiles.rds")
 
-ME_profile <- read_rds("Data/ME_YSI_2020/ME_profiles.rds") 
+ME_profile <- read_rds("Data/ME_YSI_2020/ME_profiles.rds") #July 11 data may be incorrect.. we'll reassess after next profile is in
 
 
 ggplot(ME_profile, aes(sp_cond, depth)) +
   geom_point() +
   scale_y_reverse() +
-  facet_wrap(~sampledate, ncol = 3) +
+  facet_wrap(~sampledate, ncol = 3, scales = "free_x") +
   geom_path() +
-  theme_bw() +
+  L_theme() +
   labs(x = "Specific Conductivity"~(mu~S~cm^-1)~"@ 25"*~degree*C~"\n", 
        y = "\nDepth"~(m))
 splot("conductance_time_series/", "ME_summer2020_perdate")
