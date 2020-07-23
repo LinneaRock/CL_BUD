50aeb1b8ee7ba2b588b9d1e9f93b7ca18d4b12ff
LinneaRock <lrock@wisc.edu>
2020-07-23 16:43:47 -0500

    learning a bit about lagos

--- LAGOS.R
+++ LAGOS.R
@@ -4,9 +4,37 @@ library(LAGOSNE)
 lagosne_get(dest_folder = lagos_path())
 
 
-dt <- lagosne_load()
-names(dt)
+lagos <- lagosne_load()
+names(lagos)
 
-lake_info(name = "Mendota", state = "Wisconsin")
-lake_info(name = "Monona", state = "Wisconsin")
+ME_info <- lake_info(name = "Mendota", state = "Wisconsin")
+MO_info <- lake_info(name = "Monona", state = "Wisconsin")
+
+# Get Lagos Data
+#lagos <- lagosne_load("1.087.1") #Location data 
+locus <- lagos$locus %>%
+  filter(lagoslakeid == 5371 |
+           lagoslakeid == 4559)
+
+geo <- lagos$lakes.geo %>%
+  filter(lagoslakeid == 5371 |
+           lagoslakeid == 4559)
+
+lulc <- lagos$hu12.lulc %>%
+  filter(hu12_zoneid == "HU12_12000" |
+           hu12_zoneid == "HU12_11998")
+
+
+iws = lagos$iws %>% dplyr::select(lagoslakeid,iws_nhdid,iws_ha)
+iws.lulc = lagos$iws.lulc %>% dplyr::select(lagoslakeid,iws_nlcd2011_ha_0:iws_roaddensity_density_mperha)  %>% 
+  select(-contains('_ha_'))
+iws.100 = lagos$buffer100m.lulc %>% dplyr::select(lagoslakeid,buffer100m_nlcd2011_pct_0:buffer100m_roaddensity_density_mperha) %>% 
+  select(-contains('_ha_'))
+iws.500 = lagos$buffer500m.lulc %>% dplyr::select(lagoslakeid,buffer500m_nlcd2011_pct_0:buffer500m_roaddensity_density_mperha) %>% 
+  select(-contains('_ha_'))
+lakes_limno = lagos$lakes_limno %>% dplyr::select(lagoslakeid,maxdepth)
+locus = lagos$locus %>% dplyr::select(lagoslakeid:lake_area_ha,edu_zoneid,state_zoneid)
+lakes.geo = lagos$lakes.geo %>% dplyr::select(lagoslakeid,lakeconnection,latewisconsinglaciation_glacial:wlconnections_allwetlands_contributing_area_ha)
+
+
 
