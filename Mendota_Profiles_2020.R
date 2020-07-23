a7219e4575f2995ad3cf6550e7139deef24f0633
LinneaRock <lrock@wisc.edu>
2020-07-23 17:17:36 -0500

    updating ME profile plots

--- Mendota_Profiles_2020.R
+++ Mendota_Profiles_2020.R
@@ -5,7 +5,7 @@ source("Functions/splot.R")
 source("Functions/L_theme.R")
    
 #read in the csv file rbind them and save as .rds in the git project.
-#attach <- read.csv("C:/Users/Linne/OneDrive/Documents/ME_profile_070220.csv") %>%
+#attach <- read.csv("C:/Users/Linne/OneDrive/Documents/ME_profile_071620.csv") %>%
 #  mutate(sampledate = anytime::anydate(sampledate)) #works better than lubridate in this circumstance
 
 
@@ -21,7 +21,7 @@ ME_profile <- read_rds("Data/ME_YSI_2020/ME_profiles.rds") #July 11 data may be 
 ggplot(ME_profile, aes(sp_cond, depth)) +
   geom_point() +
   scale_y_reverse() +
-  facet_wrap(~sampledate, ncol = 3, scales = "free_x") +
+  facet_wrap(~sampledate, ncol = 3) +
   geom_path() +
   L_theme() +
   labs(x = "Specific Conductivity"~(mu~S~cm^-1)~"@ 25"*~degree*C~"\n", 
@@ -35,14 +35,15 @@ ggplot(ME_profile, aes(sampledate, sp_cond, color = depth)) +
   scale_color_viridis_c(direction = -1) +
   labs(y = "Specific Conductivity"~(mu~S~cm^-1)~"@ 25"*~degree*C~"\n", 
        x = "") +
-  theme(panel.background = element_rect(fill = "white", colour = "white",
-                                        size = 2, linetype = "solid"),
-        panel.grid.major = element_line(size = 0.25, linetype = 'solid',
-                                        colour = "gray88"), 
-        panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
-                                        colour = "gray88"),
-        axis.text = element_text(size = 11),
-        axis.title = element_text(size = 11))
+  L_theme()
 
 
 splot("conductance_time_series/", "ME_summer2020_perdepth")
+
+
+#heat map? not really what I was going for
+ggplot(ME_profile %>% filter(sampledate != "2020-07-11"), aes(sampledate, depth, fill = sp_cond)) +
+  geom_tile() +
+  scale_fill_viridis_c() +
+  L_theme()+
+  scale_y_reverse()
