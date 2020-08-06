2e082369ec17a1a5aeed9e66f202de2b459f5399
LinneaRock <lrock@wisc.edu>
2020-08-06 16:10:46 -0500

    Writing code to calculate land use over different subwatersheds

---
+++ Functions/LandUse.R
@@ -0,0 +1,102 @@
+
+#Function to calculate land use percetages for each subwatershed (HUC12) area using LAGOS LULC data combined with data on the area of each subwatershed
+
+
+
+
+
+
+
+LandUse <- function(lulcdata) {
+  
+  #Calculate the area for the watershed and each land use [hectare]:
+  WatershedArea <- lulcdata %>%
+    summarise(sum(hu12_ha_in_usa))
+  
+  OpenWater <- lulcdata %>%
+    mutate(OpenWater = sum(hu12_nlcd2011_ha_11)) %>%
+    select(OpenWater) %>%
+    distinct()
+  
+  DevelopedOpenSpace <- lulcdata %>%
+    mutate(DevelopedOpenSpace = sum(hu12_nlcd2011_ha_21)) %>%
+    select(DevelopedOpenSpace) %>%
+    distinct()
+  
+  DevelopedLowIntensity <- lulcdata %>%
+    mutate(DevelopedLowIntensity = sum(hu12_nlcd2011_ha_22)) %>%
+    select(DevelopedLowIntensity) %>%
+    distinct()
+  
+  DevelopedMedIntensity <- lulcdata %>%
+    mutate(DevelopedMedIntensity = sum(hu12_nlcd2011_ha_23)) %>%
+    select(DevelopedMedIntensity) %>% 
+    distinct()
+  
+  DevelopedHighIntensity <- lulcdata %>%
+    mutate(DevelopedHighIntensity = sum(hu12_nlcd2011_ha_24)) %>%
+    select(DevelopedHighIntensity) %>%
+    distinct()
+  
+  BarrenLand <- lulcdata %>%
+    mutate(BarrenLand = sum(hu12_nlcd2011_ha_31)) %>%
+    select(BarrenLand) %>%
+    distinct()
+  
+  DeciduousForest <- lulcdata %>%
+    mutate(DeciduousForest = sum(hu12_nlcd2011_ha_41)) %>%
+    select(DeciduousForest) %>%
+    distinct()
+  
+  EvergreenForest <- lulcdata %>%
+    mutate(EvergreenForest = sum(hu12_nlcd2011_ha_42)) %>%
+    select(EvergreenForest) %>%
+    distinct()
+  
+  MixedForest <- lulcdata %>%
+    mutate(MixedForest = sum(hu12_nlcd2011_ha_43)) %>%
+    select(MixedForest) %>%
+    distinct()
+  
+  
+  ShrubScrub <- lulcdata %>%
+    mutate(ShrubScrub = sum(hu12_nlcd2011_ha_52)) %>%
+    select(ShrubScrub) %>%
+    distinct()
+  
+  GrasslandHerb <- lulcdata %>%
+    mutate(GrasslandHerb = sum(hu12_nlcd2011_ha_71)) %>%
+    select(GrasslandHerb) %>%
+    distinct()
+  
+  PastureHay <- lulcdata %>%
+    mutate(PastureHay = sum(hu12_nlcd2011_ha_81)) %>%
+    select(PastureHay) %>%
+    distinct()
+  
+  CultivatedCrop <- lulcdata %>%
+    mutate(CultivatedCrop = sum(hu12_nlcd2011_ha_82)) %>%
+    select(CultivatedCrop) %>%
+    distinct()
+  
+  WoodyWetlands <- lulcdata %>%
+    mutate(WoodyWetlands = sum(hu12_nlcd2011_ha_90)) %>%
+    select(WoodyWetlands) %>%
+    distinct()
+  
+  EmergentWetlands <- lulcdata %>%
+    mutate(EmergentWetlands = sum(hu12_nlcd2011_ha_95)) %>%
+    select(EmergentWetlands) %>%
+    distinct()
+  
+  #
+  
+  df <- OpenWater %>%
+    cbind(c(DevelopedOpenSpace, DevelopedLowIntensity, DevelopedMedIntensity, DevelopedHighIntensity, BarrenLand, DeciduousForest, EvergreenForest, MixedForest, ShrubScrub, GrasslandHerb, PastureHay, CultivatedCrop, WoodyWetlands, EmergentWetlands))
+  
+  return(df)
+  
+}
+
+
+
--- LAGOS.R
+++ LAGOS.R
@@ -18,13 +18,22 @@ locus <- lagos$locus %>%
   filter(lagoslakeid == 5371 |
            lagoslakeid == 4559)
 
+HUC12 <- lagos$hu12 %>%
+  filter(hu12_zoneid == "HU12_11991" | 
+           hu12_zoneid == "HU12_11992" |
+           hu12_zoneid == "HU12_11993" | 
+           hu12_zoneid == "HU12_11994" |
+           hu12_zoneid == "HU12_11995" |
+           hu12_zoneid == "HU12_11996" |
+           hu12_zoneid == "HU12_11997" |
+           hu12_zoneid == "HU12_11998" |
+           hu12_zoneid == "HU12_12000" | 
+           hu12_zoneid == "HU12_11999" ) %>%
+  select(hu12, hu12_ha_in_usa)
+
 geo <- lagos$lakes.geo %>%
   filter(lagoslakeid == 5371 |
            lagoslakeid == 4559)
-
-lulc <- lagos$hu12.lulc %>%
-  filter(hu12_zoneid == "HU12_12000" |
-           hu12_zoneid == "HU12_11998")
 
 conn <- lagos$hu12.conn %>%
   filter(hu12_zoneid == "HU12_12000" |
@@ -33,5 +42,52 @@ conn <- lagos$hu12.conn %>%
 chag <- lagos$hu12.chag %>%
   filter(hu12_zoneid == "HU12_12000" |
            hu12_zoneid == "HU12_11998")
+
+#Getting land use data for Mendota watershed by HUC 12
+ME <- read_rds("Data/shapefiles/HUC12_ME.rds") %>%
+  select(Name, HUC12, ZoneID)
+
+ME.HUC12.lulc <- lagos$hu12.lulc %>%
+  select(hu12_zoneid, hu12_nlcd2011_ha_0:hu12_roaddensity_density_mperha) %>%
+  filter(hu12_zoneid == "HU12_11991" | 
+           hu12_zoneid == "HU12_11992" |
+           hu12_zoneid == "HU12_11993" | 
+           hu12_zoneid == "HU12_11994" |
+           hu12_zoneid == "HU12_11995" |
+           hu12_zoneid == "HU12_11996" |
+           hu12_zoneid == "HU12_11997" |
+           hu12_zoneid == "HU12_11998" ) %>%
+  left_join(ME, c("hu12_zoneid" = "ZoneID")) %>%
+  left_join(HUC12, c("HUC12" = "hu12")) %>%
+  select(-geometry)
+
+write.csv(ME.HUC12.lulc, "Mendota.csv")
+
+#same for Monona
+MO <- read_rds("Data/shapefiles/HUC12_MO.rds") %>%
+  select(Name, HUC12, ZoneID)
+
+MO.HUC12.lulc <- lagos$hu12.lulc %>%
+  select(hu12_zoneid, hu12_nlcd2011_ha_0:hu12_roaddensity_density_mperha) %>%
+  filter(hu12_zoneid == "HU12_12000" | 
+           hu12_zoneid == "HU12_11999" ) %>%
+  left_join(MO, c("hu12_zoneid" = "ZoneID")) %>%
+  left_join(HUC12, c("HUC12" = "hu12")) %>%
+  select(-geometry)
+
+#Lake Mendota Watershed Land Use Calculations
+#calculate area of all 6 HUC12s:
+Mendota.Watershed.Area <- ME.HUC12.lulc %>%
+  summarise(sum(hu12_ha_in_usa))
+
+OpenWater <- ME.HUC12.lulc %>%
+  mutate(OpenWater = sum(hu12_nlcd2011_ha_11)) %>%
+  select(OpenWater) %>%
+  distinct()
+  
+  
+
+OpenWaterPerc <- OpenWater/Mendota.Watershed.Area * 100
+
 
 
