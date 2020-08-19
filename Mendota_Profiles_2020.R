159cac8a2206bf1710a4564a5c39538430170445
LinneaRock <lrock@wisc.edu>
2020-08-19 12:07:06 -0500

    new Mendota profile

--- Mendota_Profiles_2020.R
+++ Mendota_Profiles_2020.R
@@ -5,13 +5,13 @@ source("Functions/splot.R")
 source("Functions/L_theme.R")
    
 #read in the csv file rbind them and save as .rds in the git project.
-#attach <- read.csv("C:/Users/Linne/OneDrive/Documents/ME_profile_080520.csv") %>%
+#attach <- read.csv("C:/Users/Linne/OneDrive/Documents/ME_profile_081820.csv") %>%
 #  mutate(sampledate = anytime::anydate(sampledate)) #works better than lubridate in this circumstance
 
 
 #ME_profile <- read_rds("Data/ME_YSI_2020/ME_profiles.rds") 
 
-#attach2 <- rbind(ME_profile, attach) #be sure that the previous version of ME_profiles is loaded
+#attach2 <- bind_rows(ME_profile, attach) #be sure that the previous version of ME_profiles is loaded
 
 #write_rds(attach2, "Data/ME_YSI_2020/ME_profiles.rds")
 
--- storm_sewer_PHMDC.R
+++ storm_sewer_PHMDC.R
@@ -4,7 +4,7 @@ library(sf)
 library(rgdal)
 
 idde <- read_xlsx("Data/Historical_External/IDDE.xlsx")
-sewer_shape <- readOGR("C:/Users/linne/Downloads/Storm_Sewer_Pipes-shp/Storm_Sewer_Pipes.shp") 
+
 
 
 
