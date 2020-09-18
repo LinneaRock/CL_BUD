8484322c9287843e9f4a99bf27c4365868e47952
LinneaRock <lrock@wisc.edu>
2020-09-18 17:37:00 -0500

    new profile data for ME

--- Mendota_Profiles_2020.R
+++ Mendota_Profiles_2020.R
@@ -5,8 +5,8 @@ source("Functions/splot.R")
 source("Functions/L_theme.R")
    
 #read in the csv file rbind them and save as .rds in the git project.
-#attach <- read.csv("C:/Users/Linne/OneDrive/Documents/ME_profile_091120.csv") %>%
- # mutate(sampledate = anytime::anydate(sampledate)) #works better than lubridate in this circumstance
+#attach <- read.csv("C:/Users/Linne/OneDrive/Documents/ME_profile_091720.csv") %>%
+#  mutate(sampledate = anytime::anydate(sampledate)) #works better than lubridate in this circumstance
 
 
 #ME_profile <- read_rds("Data/ME_YSI_2020/ME_profiles.rds") 
