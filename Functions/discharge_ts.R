c8d0b778f28c0de14927d55f3991f78d51b20507
LinneaRock <lrock@wisc.edu>
2020-11-30 18:56:36 -0600

    need to fix code...

--- Functions/discharge_ts.R
+++ Functions/discharge_ts.R
@@ -1,3 +1,15 @@
+
+#6-hour moving average for discharge data
+rolling_ave_discharge <- function(discharge_data) {
+  
+  dis_data2 <- discharge_data %>%
+    mutate(runningmean = rollmean(discharge, 13, fill = NA, na.rm = TRUE)) %>% #use zoo::rollmean over 13 rows (6 hours - 3 before and 3 after each point)
+    mutate(runningmean = ifelse(row_number() <= 6, mean(discharge[1:6]), runningmean)) %>% # rollmean leaves empty rows at beginning and end of dataset. This line and the one below uses the mean of those empty rows
+    mutate(runningmean = ifelse(row_number() >= (nrow(discharge_data) - 5), mean(discharge[(nrow(discharge_data) - 5):nrow(discharge_data)]), runningmean))
+  
+}
+
+#plot timeseries discharge data
 discharge_ts <- function(dischargedata) {
   ggplot(dischargedata) +
     geom_line(aes(date, discharge)) +
@@ -5,3 +17,8 @@ discharge_ts <- function(dischargedata) {
     labs(x = "",
          y = "Discharge (cms)")
 }
+
+
+
+
+
