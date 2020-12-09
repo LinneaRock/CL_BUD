d5305ac6ca084fc999af7a528d6ebf9e1733e965
LinneaRock <lrock@wisc.edu>
2020-12-09 14:54:34 -0600

    imputing missing values

--- Tributaries/PheasantBranch_SouthFork.R
+++ Tributaries/PheasantBranch_SouthFork.R
@@ -18,31 +18,30 @@ source("Functions/qsc.R")
 source("Functions/qcl.R")
 source("Functions/discharge_ts.R")
 
-#looking at raw data to find when logger was logging outside of the stream
-# pbsf <- loggerPBSF %>%
-#   mutate(Year_Month = paste(year(date), month(date), sep = "-"), #year month 
-#          YMD = paste(year(date), month(date), day(date), sep = "-")) 
-# ggplot(pbsf) +
-#   geom_point(aes(date, Full.Range)) +
-#   facet_wrap("Year_Month", scales = "free")
-# 
-# pbsf_dis <- PBSF_discharge %>%
-#   mutate(Year_Month = paste(year(date), month(date), sep = "-"), #year month 
-#          YMD = paste(year(date), month(date), day(date), sep = "-")) %>%
-#   filter(date <= "2020-11-01 00:00:00")
-# 
-# ggplot(pbsf_dis) +
-#   geom_point(aes(date, discharge)) +
-#   facet_wrap("Year_Month", scales = "free")
 
-#calling and naming raw data
-loggerPBSF2 <- loggerPBSF %>%  #HOBO conductivity data - when water is low, sensor is not under water. Adding NAs where this occurs 
-  mutate(sp.cond = ifelse(date >= "2020-04-18 17:30:00" & date <= "2020-04-22 18:00:00", NA, sp.cond)) %>%  
-  mutate(sp.cond = ifelse(date >= "2020-05-05 21:30:00" & date <= "2020-05-13 21:00:00", NA, sp.cond)) %>%
-  mutate(sp.cond = ifelse(date >= "2020-07-31 19:30:00" & date <= "2020-08-15 9:00:00", NA, sp.cond)) %>%
-  mutate(sp.cond = ifelse(date >= "2020-08-18 17:00:00" & date <= "2020-08-26 12:30:00", NA, sp.cond)) %>%
+
+#getting conductivity data ready
+loggerPBSF1 <- loggerPBSF %>%  #HOBO conductivity data - when water is low, sensor is not under water. Adding NAs where this occurs 
+  # mutate(sp.cond = ifelse(date >= "2020-04-18 17:30:00" & date <= "2020-04-22 18:00:00", NA, sp.cond)) %>%  
+  # mutate(sp.cond = ifelse(date >= "2020-05-05 21:30:00" & date <= "2020-05-13 21:00:00", NA, sp.cond)) %>%
+  # mutate(sp.cond = ifelse(date >= "2020-07-31 19:30:00" & date <= "2020-08-15 9:00:00", NA, sp.cond)) %>%
+  # mutate(sp.cond = ifelse(date >= "2020-08-18 17:00:00" & date <= "2020-08-26 12:30:00", NA, sp.cond)) %>%
+  mutate(sp.cond = ifelse(sp.cond < 200, NA, sp.cond)) %>%
   complete(date = seq.POSIXt(as.POSIXct("2020-01-15 13:30:00"), as.POSIXct("2020-01-21 16:30:00"), by = "30 mins")) %>%
   arrange(date)
+
+to_imp <- loggerPBSF1 %>%
+  select(date, sp.cond) %>%
+  rename(imputed = sp.cond)
+
+loggerPBSF2 <- to_imp %>%  
+  as.ts() %>%
+  na_ma(6, "exponential") %>%
+  as.data.frame() %>%
+  mutate(date = as.POSIXct(date, format = "%Y-%m-%d %H:%M:%S", origin = "1970-01-01 00:00:00", tz = "GMT")) %>%
+  left_join(loggerPBSF1, by = "date") %>%
+  mutate(imputed = ifelse(is.na(sp.cond), imputed, NA)) %>%
+  mutate(sp.cond = ifelse(is.na(sp.cond), imputed, sp.cond))
 
 
 
@@ -57,6 +56,7 @@ PBSF_cond_data <- find_outlier(loggerPBSF2, fieldcondPBSF, "PBSFoutliers", "PBSF
 
 #Conductivity time series
 PBSF_cond_plot <- cond(PBSF_cond_data) +
+  geom_line(aes(date, imputed), color = "red") +
   capt_scseries("Pheasant Branch South Fork", "Pheasant Branch South Fork")
 splot("conductance_time_series/", "PBSF")
 
@@ -100,50 +100,4 @@ cond_compare(fieldcondPBSF, loggerPBSF)
 cl_compare(fieldclPBSF, labPBSF)
 
 
-#trying to impute values
-impute_PBSF_cond <- loggerPBSF3 %>%
-  #select(date, sp.cond) %>%
-  as.ts()
-
-imps <- na_ma(impute_PBSF_cond, 6, "exponential")
-
-imps2 <- as.data.frame(imps)  %>%
-  mutate(date = as.POSIXct(date, format = "%Y-%m-%d %H:%M:%S", origin = "1970-01-01 00:00:00", tz = "GMT")) #%>%
-  #rename(imputed = runningmean)
-
-ggplot(imps2,
-       aes(date, sp.cond)) +
-  geom_point()
-   
-
-test <- PBSF_cond_data %>%
-  left_join(imps2, by = "date") %>%
-  mutate(imputed = ifelse(is.na(corr_sp.cond), imputed, NA))
-
-ggplot(test) +
-  geom_point(aes(date, corr_sp.cond)) +
-  geom_point(aes(date, imputed), color = "red")
-
-test2 <- find_outlier(imps2, fieldcondPBSF, "test1", "test2")
-
-finder <- join_datasets_chloride(labDC, imps2)
-
-loggerPBSF3 <- loggerPBSF2 %>%
-  complete(date = seq.POSIXt(as.POSIXct("2020-01-15 13:30:00"), as.POSIXct("2020-01-21 16:30:00"), by = "30 mins")) %>%
-  arrange(date)
-
-loggerPBSF <- loggerPBSF %>%  #HOBO conductivity data - when water is low, sensor is not under water. Adding NAs where this occurs 
-  mutate(sp.cond = ifelse(date >= "2020-04-18 17:30:00" & date <= "2020-04-22 18:00:00", NA, sp.cond)) %>%  
-  mutate(sp.cond = ifelse(date >= "2020-05-05 21:30:00" & date <= "2020-05-13 21:00:00", NA, sp.cond)) %>%
-  mutate(sp.cond = ifelse(date >= "2020-07-31 19:30:00" & date <= "2020-08-15 9:00:00", NA, sp.cond)) %>%
-  mutate(sp.cond = ifelse(date >= "2020-08-18 17:00:00" & date <= "2020-08-26 12:30:00", NA, sp.cond)) %>%
-  complete(date = seq.POSIXt(as.POSIXct("2020-01-15 13:30:00"), as.POSIXct("2020-01-21 16:30:00"), by = "30 mins")) %>%
-  arrange(date) 
-loggerPBS2 <- loggerPBSF %>%  
-  as.ts() %>%
-  na_ma(6, "exponential") %>%
-  as.data.frame() %>%
-  mutate(date = as.POSIXct(date, format = "%Y-%m-%d %H:%M:%S", origin = "1970-01-01 00:00:00", tz = "GMT")) %>%
-  left_join(loggerPBSF, by = "date") %>%
-  mutate(imputed = ifelse(is.na(sp.cond.y), sp.cond.x, NA))
 
