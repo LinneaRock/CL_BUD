6b397bb13e8a8c81cf02495c9da8220456ea6eb5
LinneaRock <lrock@wisc.edu>
2021-02-09 18:34:14 -0600

    working on method to separate out events in hydrograph

--- flow_events.R
+++ flow_events.R
@@ -16,7 +16,10 @@ hydrograph(input = DC_discharge, streamflow2 = baseflow_DC[,1])
 
 detach(package:EcoHydRology)
 
+
 event_DC <- cbind(DC_discharge, baseflow_DC) 
+
+
 
 event_DC2 <- event_DC %>%
   mutate(diff = discharge - bt) %>%
@@ -25,12 +28,81 @@ event_DC2 <- event_DC %>%
 
 
 
-ggplot(event_DC2 %>% filter(test == 0.25)) +
+ggplot(event_DC2) +
   geom_point(aes(date, discharge)) +
-  geom_point(aes(date, bt),color = "red") #+
-  #geom_line(aes(date, test), color = "blue")
+  geom_point(aes(date, bt),color = "red") +
+  geom_point(aes(date, test), color = "blue")
 
 mean(event_DC$discharge) * 1000
+
+#combine discharge and conductivity
+DC_event_cond <- event_DC2 %>%
+  left_join(DC_cond_data, by = "date") %>%
+  mutate(mon = months.POSIXt(date)) %>%
+  mutate(year = year(date)) 
+
+
+#prepare precip data to only have dates before, after, and including precip events
+precip <- precip_temp_data %>%
+  mutate(rain = ifelse(PRCP == 0, "N", "Y")) %>%
+  mutate(keep = ifelse(lag(rain, n = 1L) == "Y" | lag(rain, n = 2L) == "Y" | PRCP > 0 | lead(rain) == "Y", 1, 0)) %>%
+  filter(keep == 1)
+
+testonly <- DC_event_cond %>%
+  select(date, test) %>%
+  filter(test == 0.25) %>%
+  mutate(date2 = as.Date(date)) %>%
+  select(date2, test) %>%
+  distinct()
+
+precip2 <- precip %>%
+  left_join(testonly, by = c("date" = "date2")) %>%
+  mutate(keep2 = ifelse(lag(test, n = 1L) == 0.25 | lag(test, n = 2L) == 0.25 | lead(test, n = 2L) == 0.25 | lead(test, n = 1L) == 0.25 | test == 0.25, 1, 0)) %>%
+  filter(keep2 == 1)
+
+#conmbine discharge/cond with precip. Keep only day prior to rain event, during rain event, and day after rain event
+DC_event_cond <- DC_event_cond %>%
+  mutate(date2 = as.Date(date)) %>%
+  left_join(precip2, by = c("date2" = "date")) 
+
+DC_event_cond2 <- DC_event_cond %>%
+  filter(keep2 == 1) %>%
+  select(date, discharge, runningmeandis, runningmean, PRCP, mon, year, bt, qft, diff, date2, test.x)
+
+DC_next <- DC_event_cond %>%
+  mutate(minus1 = date2 - 86400) %>%
+  mutate(plus1 = date2 + 86400) %>%
+  mutate(plus2 = date2 + 86400 + 86400) %>%
+  mutate(event = ifelse(is.na(test), "N", "Y")) %>%
+  mutate(pre_event = ifelse(event == "Y" & 
+                              date2 == minus1, "Y", "N")) %>%
+  select(date, discharge, runningmeandis, runningmean, PRCP, mon, year, bt, qft, diff, date2, test, minus1, plus1, plus2, event, pre_event)
+
+
+
+dates <- as.vector(as.character(date_group$date2))
+dates1 <- sort(as.Date((dates)))
+split(dates1, cumsum(c(TRUE, diff(dates1) != 1)))
+df1 <- data.frame(dates1, group = cumsum(c(TRUE, diff(dates1) != 1))) 
+
+
+date_group <- DC_event_cond2 %>%
+  left_join(df1, by = c("date2" = "dates1"))
+
+
+ggplot(date_group) +
+  geom_point(aes(date, runningmeandis)) +
+  geom_point(aes(date, bt),color = "red") +
+  geom_point(aes(date, test.x), color = "blue") +
+  facet_wrap("group", scales = "free")
+
+
+mean(DC_discharge$runningmeandis) #0.4644761
+median(DC_discharge$runningmeandis) #0.3859804
+
+ggplot(DC_next) + geom_line(aes(date, ldis))
+
+
 
 flow_prcp <- DC_discharge %>%
   separate(date, c("date", "time"), sep = " ") %>%
@@ -43,3 +115,39 @@ flow_prcp <- DC_discharge %>%
 
 
 ggplot(flow_prcp) + geom_point(aes(PRCP, meandis))
+
+
+
+
+
+
+
+
+dates <- c("2018-05-16", "2018-07-20", "2018-06-25", "2018-06-26", "2018-07-21", "2018-07-22", "2018-05-17")
+
+dates1 <- sort(as.Date(dates))
+split(dates1, cumsum(c(TRUE, diff(dates1) != 1)))
+
+
+df1 <- data.frame(dates1, group = cumsum(c(TRUE, diff(dates1) != 1)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
