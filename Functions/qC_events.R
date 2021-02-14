e840afe38100705dd2cccbe3cb5d8de2d2cdef56
LinneaRock <lrock@wisc.edu>
2021-02-13 18:00:13 -0600

    working on QC plots now. tricky visualization to figure out next (inspo from Knapp et al., 2020)

---
+++ Functions/qC_events.R
@@ -0,0 +1,176 @@
+library(tidyverse)
+library(lubridate)
+library(broom)
+library(gt)
+library(webshot)
+source("Functions/L_theme.R")
+
+
+#part1
+find_all_events <- function(discharge_data, baseflow_data, fileName, cond_data) {
+
+  threshold <- mean(discharge_data$runningmeandis)/2 #the difference between the the peak event discharge and the baseflow must be at least half the total average discharge. Adapted from Knapp et al., 2020
+  
+  
+  df <- cbind(discharge_data, baseflow_data) %>%
+    mutate(difference = runningmeandis - bt) %>%
+    mutate(test = ifelse(difference > threshold, 0.25, NA)) #this just helps narrow down the dataset right away
+  
+ #saving a plot to check for problems 
+  ggplot(df) +
+    geom_point(aes(date, runningmeandis)) +
+    geom_point(aes(date, bt),color = "red") +
+    geom_point(aes(date, test), color = "blue")
+  ggsave(paste("Plots/QC_plots/", fileName, "/checkfolder/checkplot.png", sep = ""))
+
+#combine discharge data with conductivity data
+df1 <- df %>%
+  left_join(cond_data, by = "date") %>%
+  mutate(mon = months.POSIXt(date)) %>%
+  mutate(year = year(date))
+
+#specifying 1 day pre rain and 2 days post rain
+precip <- precip %>%
+  mutate(rain = ifelse(PRCP == 0, "N", "Y")) %>%
+  mutate(keep = ifelse(lag(rain, n = 1L) == "Y" | lag(rain, n = 2L) == "Y" | PRCP > 0 | lead(rain) == "Y", 1, 0)) %>%
+  filter(keep == 1)
+
+#intermediary step
+temporary <- df1 %>%
+  select(date, test) %>%
+  filter(test == 0.25) %>%
+  mutate(date2 = as.Date(date)) %>%
+  select(date2, test) %>%
+  distinct()
+
+#Noting which dates of data to keep (i.e., before and after a rain event and flow exceeds threshold)
+precip2 <- precip %>%
+  left_join(temporary, by = c("date" = "date2")) %>%
+  mutate(keep2 = ifelse(lag(test, n = 1L) == 0.25 | lag(test, n = 2L) == 0.25 | lead(test, n = 2L) == 0.25 | lead(test, n = 1L) == 0.25 | test == 0.25, 1, 0)) %>%
+  filter(keep2 == 1)
+
+
+#conmbine discharge/cond with precip. Keep only day prior to rain event, during rain event, and day after rain event
+df2 <- df1 %>%
+  mutate(date2 = as.Date(date)) %>%
+  left_join(precip2, by = c("date2" = "date")) %>%
+  filter(keep2 == 1) %>%
+  select(date, discharge, runningmeandis, runningmean, PRCP, mon, year, bt, qft, difference, date2, test.x)
+
+
+#create groupings based on dates of events
+dates_only <- df2 %>%
+  select(date2) %>%
+  unique()
+dates <- as.vector(as.character(dates_only$date2))
+dates1 <- sort(as.Date((dates)))
+split(dates1, cumsum(c(TRUE, diff(dates1) != 1)))
+dates2 <- data.frame(dates1, group = cumsum(c(TRUE, diff(dates1) != 1))) 
+
+ #putting the date groups into the dataset
+ date_group <- df2 %>%
+   left_join(dates2, by = c("date2" = "dates1"))
+
+ #save a plot to check for problems
+ ggplot(date_group) +
+   geom_point(aes(date, runningmeandis)) +
+   geom_point(aes(date, bt),color = "red") +
+   geom_point(aes(date, test.x), color = "blue") +
+   facet_wrap("group", scales = "free")
+ ggsave(paste("Plots/QC_plots/", fileName, "/checkfolder/flowevents.png", sep = ""), height = 12, width = 12, units = "in")
+
+ #save a plot of conductivity. This and previous plot need to be checked to ensure we have both conductivity data and flow data at each event that we want to use
+ ggplot(date_group) +
+   geom_point(aes(date, runningmean)) +
+  facet_wrap("group", scales = "free")
+ ggsave(paste("Plots/QC_plots/", fileName, "/checkfolder/cond.png", sep = ""), height = 12, width = 12, units = "in")
+
+ return(date_group)
+ 
+}
+
+
+#part2
+
+QC_plots <- function(x,y, event_data, fileName, creekName) {
+  
+  labels <- as.data.frame(x) %>%
+    mutate(y = y)
+  
+  final <- event_data %>%
+    left_join(labels, by = c("group" = "x"))
+  
+  
+  ggplot(final %>% filter(group == x)) +
+    geom_point(aes(runningmeandis, runningmean)) +
+    geom_smooth(aes(runningmeandis, runningmean), method = "lm", se = FALSE, color = "#1DACE8") +
+    labs(y = "Specific Conductivity"~(mu~S~cm^-1)~"@ 25"*~degree*C~"\n", 
+         x = "\nDischarge"~(m^3~s^-1)) +
+    L_theme() +
+    facet_wrap("y", scales = "free")
+  ggsave(paste("Plots/QC_plots/", fileName, "/regressions.png", sep = ""), height = 12, width = 12, units = "in")
+  
+  
+  ggplot(final %>% filter(group == x)) +
+    geom_line(aes(date, runningmean, color = "#F24D29")) +
+    labs(x = "",
+         y = "Specific Conductivity"~(mu~S~cm^-1)~"@ 25"*~degree*C~"\n") +
+    L_theme() + theme(legend.position = "none") +
+    facet_wrap("y", scales = "free") 
+  ggsave(paste("Plots/QC_plots/", fileName, "/conductivity.png", sep = ""), height = 12, width = 12, units = "in")
+  
+  
+  ggplot(final %>% filter(group == x)) +
+    geom_line(aes(date, runningmeandis, color = "Flow (cms)")) +
+    geom_line(aes(date, bt, color = "Baseflow (cms)")) +
+    facet_wrap("y", scales = "free") +
+    labs(x = "", y = "\nDischarge"~(m^3~s^-1)) +
+    L_theme() +
+    scale_color_manual("",
+                       labels = c("Baseflow", "Flow"),
+                       values = c("#C4CFD0", "#1C366B")) 
+  ggsave(paste("Plots/QC_plots/", fileName, "/discharge.png", sep = ""), height = 12, width = 12, units = "in")
+  
+  
+  df <- event_data %>%
+    #na.omit() %>%
+    #filter(group == x)
+    nest(data = -group) %>% 
+    mutate(
+      fit = map(data, ~ lm(runningmean~runningmeandis, data = .x)),
+      tidied = map(fit, tidy),
+      glanced = map(fit, glance),
+      augmented = map(fit, augment)
+    ) %>% 
+    unnest(tidied)
+  
+  df1 <- df %>%
+    select(group, term, estimate, glanced) 
+  
+  df2 <- df1 %>%
+    unnest(glanced) %>%
+    left_join(labels, by = c("group" = "x")) %>%
+    pivot_wider(names_from = term, values_from = estimate) %>%
+    rename("slope" = "runningmeandis",
+           "intercept" = "(Intercept)") %>%
+    select(y, adj.r.squared, intercept, slope) %>%
+    drop_na(y)
+  
+  
+  gt_tbl <- gt(df2)
+  table <- gt_tbl %>%
+    cols_label(
+      y = "Event",
+      slope = "Slope",
+      intercept = "Intercept",
+      adj.r.squared = "R Squared"
+    ) %>%
+    tab_header(
+      title = paste("Regression stats for", creekName, "events", sep = ""),
+    ); table
+  
+  # whitespace can be set, zoom sets resolution
+  gtsave(data = table, paste("Plots/QC_plots/", fileName, "/stats.png", sep = ""), expand = 10, zoom = 10)
+  
+  
+}
--- flow_events.R
+++ flow_events.R
@@ -1,209 +1,34 @@
-library(tidyverse)
-library(lubridate)
 
+precip <- read.csv("Data/Historical_External/precip_temp.csv") %>%
+  mutate(PRCP = PRCP * 25.4) %>%  #inches to mm
+  mutate(date = as.POSIXct(as.character(DATE)))
 
-library(EcoHydRology)
+DC_discharge <- read_rds("Data/discharge/DC_discharge.rds")
+DC_cond_data <- read_rds("Data/HOBO_Loggers/DC/DC_cond_data.rds")
 
-baseflow_DC <- BaseflowSeparation(DC_discharge$discharge, filter_parameter = 0.925, passes = 3) %>%
-  mutate(obs = 1:n())
 
-ggplot(baseflow_DC) + geom_line(aes(x = obs, y = bt)) + geom_line(aes(x = obs, y = qft), color = "red")
+PBSF_discharge <- read_rds("Data/discharge/PBSF_discharge.rds")
+PBSF_cond_data <- read_rds("Data/HOBO_Loggers/PBSf/PBSF_cond_data.rds")
 
-hydrograph(input = DC_discharge, streamflow2 = baseflow_DC[,1])
+#Get the baseflow information using the package of EcoHydRology. Then restart R before continuing.
+#library(EcoHydRology)
 
+DC_baseflow <- BaseflowSeparation(DC_discharge$runningmeandis, filter_parameter = 0.925, passes = 3) 
+PBSF_baseflow <- BaseflowSeparation(PBSF_discharge$runningmeandis, filter_parameter = 0.925, passes = 3)
 
-??EcoHydRology::BaseflowSeparation
 
-detach(package:EcoHydRology)
+source("Functions/qC_events.R")
 
+DC_event <- find_all_events(DC_discharge, DC_baseflow, "DC", DC_cond_data)
+#check the plots in the folder
+x <- c(1, 4:6, 8, 10, 11, 13, 15, 16, 18, 20, 22, 23)
+y <- LETTERS[1:14]
 
-event_DC <- cbind(DC_discharge, baseflow_DC) 
+QC_plots(x, y, DC_event, "DC", "Dorn Creek")
 
 
+PBSF_event <- find_all_events(PBSF_discharge, PBSF_baseflow, "PBSF", PBSF_cond_data)
+x <- c(1, 2, 4, 6, 8, 11, 12, 13, 15, 18, 21)
+y <- LETTERS[1:11]
 
-event_DC2 <- event_DC %>%
-  mutate(diff = discharge - bt) %>%
-  mutate(diff = diff * 1000) %>% 
-  mutate(test = ifelse(diff > 200, 0.25, NA))
-
-
-
-ggplot(event_DC2) +
-  geom_point(aes(date, discharge)) +
-  geom_point(aes(date, bt),color = "red") +
-  geom_point(aes(date, test), color = "blue")
-
-mean(event_DC$discharge) * 1000
-
-#combine discharge and conductivity
-DC_event_cond <- event_DC2 %>%
-  left_join(DC_cond_data, by = "date") %>%
-  mutate(mon = months.POSIXt(date)) %>%
-  mutate(year = year(date)) 
-
-
-#prepare precip data to only have dates before, after, and including precip events
-precip <- precip_temp_data %>%
-  mutate(rain = ifelse(PRCP == 0, "N", "Y")) %>%
-  mutate(keep = ifelse(lag(rain, n = 1L) == "Y" | lag(rain, n = 2L) == "Y" | PRCP > 0 | lead(rain) == "Y", 1, 0)) %>%
-  filter(keep == 1)
-
-testonly <- DC_event_cond %>%
-  select(date, test) %>%
-  filter(test == 0.25) %>%
-  mutate(date2 = as.Date(date)) %>%
-  select(date2, test) %>%
-  distinct()
-
-precip2 <- precip %>%
-  left_join(testonly, by = c("date" = "date2")) %>%
-  mutate(keep2 = ifelse(lag(test, n = 1L) == 0.25 | lag(test, n = 2L) == 0.25 | lead(test, n = 2L) == 0.25 | lead(test, n = 1L) == 0.25 | test == 0.25, 1, 0)) %>%
-  filter(keep2 == 1)
-
-#conmbine discharge/cond with precip. Keep only day prior to rain event, during rain event, and day after rain event
-DC_event_cond <- DC_event_cond %>%
-  mutate(date2 = as.Date(date)) %>%
-  left_join(precip2, by = c("date2" = "date")) 
-
-DC_event_cond2 <- DC_event_cond %>%
-  filter(keep2 == 1) %>%
-  select(date, discharge, runningmeandis, runningmean, PRCP, mon, year, bt, qft, diff, date2, test.x)
-
-DC_next <- DC_event_cond %>%
-  mutate(minus1 = date2 - 86400) %>%
-  mutate(plus1 = date2 + 86400) %>%
-  mutate(plus2 = date2 + 86400 + 86400) %>%
-  mutate(event = ifelse(is.na(test), "N", "Y")) %>%
-  mutate(pre_event = ifelse(event == "Y" & 
-                              date2 == minus1, "Y", "N")) %>%
-  select(date, discharge, runningmeandis, runningmean, PRCP, mon, year, bt, qft, diff, date2, test, minus1, plus1, plus2, event, pre_event)
-
-
-
-dates <- as.vector(as.character(date_group$date2))
-dates1 <- sort(as.Date((dates)))
-split(dates1, cumsum(c(TRUE, diff(dates1) != 1)))
-df1 <- data.frame(dates1, group = cumsum(c(TRUE, diff(dates1) != 1))) 
-
-
-date_group <- DC_event_cond2 %>%
-  left_join(df1, by = c("date2" = "dates1"))
-
-
-ggplot(date_group) +
-  geom_point(aes(date, runningmeandis)) +
-  geom_point(aes(date, bt),color = "red") +
-  geom_point(aes(date, test.x), color = "blue") +
-  facet_wrap("group", scales = "free")
-
-
-mean(DC_discharge$runningmeandis) * 2 #0.4644761
-median(DC_discharge$runningmeandis) #0.3859804
-
-
-check_max <- date_group %>%
-  group_by(group) %>%
-  mutate(peak = max(runningmeandis)) %>%
-  ungroup() %>%
-  mutate(is.greater = ifelse(peak >= (2 * mean(DC_discharge$runningmeandis)), "keep", "don't")) %>%
-  filter(is.greater == "keep")
-
-
-ggplot(check_max %>% filter(group == c(1, 6, 8, 18, 23, 25, 26))) +
-  geom_point(aes(date, runningmeandis)) +
-  geom_point(aes(date, bt),color = "red") +
-  #geom_point(aes(date, test.x), color = "blue") +
-  facet_wrap("group", scales = "free")
-
-
-
-
-x <- c(1, 4:6, 8, 10, 12:14, 16:18, 23, 26, 27)
-y <- LETTERS[1:15]
-
-labels <- as.data.frame(x) %>%
-  mutate(y = y)
-
-#function to obtain coefficient information from regression
-info <- function() {
-  
-  info <- lm(runningmean ~ runningmeandis, date_group)
-
- 
-  coef(info())[2]
-
-}
-
-summary(lm(runningmean~runningmeandis, date_group))$adj.r.squared
-
-date_group <- date_group %>%
-  left_join(labels, by = c("group" = "x"))
-  #mutate(slope = lapply(split(date_group, group), lm(runningmean ~ runningmeandis)))
-
-
-for(i in (1:length(date_group$group))) {
-  
-  test <- date_group %>%
-    mutate(slope = coef(lm(runningmean~runningmeandis))[2]) %>%
-    mutate(r.sq = summary(lm(runningmean~runningmeandis))$adj.r.squared)
-  
-}
-  
-
-
- ggplot(date_group %>% filter(group == c(1, 4:6, 8, 10, 12:14, 16:18, 23, 26, 27))) +
-  geom_point(aes(runningmeandis, runningmean)) +
-  geom_smooth(aes(runningmeandis, runningmean), method = "lm", se = FALSE, color = "#1DACE8") +
-  labs(y = "Specific Conductivity"~(mu~S~cm^-1)~"@ 25"*~degree*C~"\n", 
-       x = "\nDischarge"~(m^3~s^-1)) +
-  L_theme() +
-  facet_wrap("y", scales = "free")
-
-
-
-ggplot(date_group %>% filter(group == c(1, 4:6, 8, 10, 12:14, 16:18, 23, 26, 27))) +
-  geom_line(aes(date, runningmean, color = "#F24D29")) +
-  labs(x = "",
-       y = "Specific Conductivity"~(mu~S~cm^-1)~"@ 25"*~degree*C~"\n") +
-  L_theme() + theme(legend.position = "none") +
-  facet_wrap("y", scales = "free") 
-
-
-
-ggplot(date_group %>% filter(group == c(1, 4:6, 8, 10, 12:14, 16:18, 23, 26, 27))) +
-  geom_line(aes(date, runningmeandis, color = "Flow (cms)")) +
-  geom_line(aes(date, bt, color = "Baseflow (cms)")) +
-  facet_wrap("y", scales = "free") +
-  labs(x = "", y = "\nDischarge"~(m^3~s^-1)) +
-  L_theme() +
-  scale_color_manual("",
-                     labels = c("Baseflow", "Flow"),
-                     values = c("#C4CFD0", "#1C366B")) 
-
-
-
-
-
-x <- c(1, 4:6, 8, 10, 12:14, 16:18, 23, 26, 27)
-y <- LETTERS[1:15]
-
-labels <- as.data.frame(x) %>%
-  mutate(y = y)
-
-date_group <- date_group %>%
-  left_join(labels, by = c("group" = "x"))
-
-
-
-
-
-df_subs <- lapply(unique(date_group$group), function(i){
-  date_group[date_group$group ==  i,] %>%
-    mutate(slope = coef(lm(runningmean~runningmeandis))[2]) %>%
-    mutate(r.sq = summary(lm(runningmean~runningmeandis))$adj.r.squared)
-}) 
-
-
-
-
-
+QC_plots(x, y, PBSF_event, "PBSF", "Pheasant Branch South Fork")
