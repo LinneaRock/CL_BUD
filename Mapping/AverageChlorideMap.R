0c529c07cdbdd2f1036d91371faeb6664fca4954
LinneaRock <lrock@wisc.edu>
2021-04-19 20:41:11 -0500

    working on figs for presentation

--- Mapping/AverageChlorideMap.R
+++ Mapping/AverageChlorideMap.R
@@ -6,86 +6,118 @@ library(viridisLite)
 
 
 ME.sf <- read_rds("Data/shapefiles/ME.rds") %>%
-  mutate(chloride = mean(labME$chloride_mgL))
+  mutate(chloride_alltime = mean(labME$chloride_mgL, na.rm = TRUE),
+         chloride_summer = mean((labME %>% filter(season == "April - October"))$chloride_mgL, na.rm = TRUE),
+         chloride_winter = mean((labME %>% filter(season == "November - March"))$chloride_mgL, na.rm = TRUE))
 
 MO.sf <- read_rds("Data/shapefiles/MO.rds") %>%
-  mutate(chloride = mean(labMO$chloride_mgL))
+  mutate(chloride_alltime = mean(labMO$chloride_mgL, na.rm = TRUE),
+         chloride_summer = mean((labMO %>% filter(season == "April - October"))$chloride_mgL, na.rm = TRUE),
+         chloride_winter = mean((labMO %>% filter(season == "November - March"))$chloride_mgL, na.rm = TRUE))
 
-YN.sf <- read_rds("Data/shapefiles/YN.rds") %>%
-  mutate(chloride = mean(labYN$chloride_mgL, na.rm = TRUE)) %>%
+
+YN.sf <- read_rds("Data/shapefiles/YN/YN.rds") %>%
+  mutate(chloride_alltime = mean(labYN$chloride_mgL, na.rm = TRUE),
+         chloride_summer = mean((labYN %>% filter(season == "April - October"))$chloride_mgL, na.rm = TRUE),
+         chloride_winter = mean((labYN %>% filter(season == "November - March"))$chloride_mgL, na.rm = TRUE)) %>%
   rename(NAME = RIVER_SYS_) %>%
   mutate(NAME = "Yahara River North")
 
-YI.sf <- read_rds("Data/shapefiles/YI.rds")  %>%
-  mutate(chloride = mean(labYI$chloride_mgL, na.rm = TRUE)) %>%
+YI.sf <- read_rds("Data/shapefiles/YI/YI.rds")  %>%
+  mutate(chloride_alltime = mean(labYI$chloride_mgL, na.rm = TRUE),
+         chloride_summer = mean((labYI %>% filter(season == "April - October"))$chloride_mgL, na.rm = TRUE),
+         chloride_winter = mean((labYI %>% filter(season == "November - March"))$chloride_mgL, na.rm = TRUE))%>%
   rename(NAME = RIVER_SYS_) %>%
   mutate(NAME = "Yahara River Isthmus")
 
-YS.sf <- read_rds("Data/shapefiles/YS.rds") %>%
-  mutate(chloride = mean(labYS$chloride_mgL, na.rm = TRUE)) %>%
+YS.sf <- read_rds("Data/shapefiles/YS/YS.rds") %>%
+  mutate(chloride_alltime = mean(labYS$chloride_mgL, na.rm = TRUE),
+         chloride_summer = mean((labYS %>% filter(season == "April - October"))$chloride_mgL, na.rm = TRUE),
+         chloride_winter = mean((labYS %>% filter(season == "November - March"))$chloride_mgL, na.rm = TRUE))%>%
   rename(NAME = RIVER_SYS_) %>%
   mutate(NAME = "Yahara River South")
 
-SMC.sf <- read_rds("Data/shapefiles/SMC.rds") %>%
-  mutate(chloride = mean(lab6MC$chloride_mgL, na.rm = TRUE))%>%
+SMC.sf <- read_rds("Data/shapefiles/SMC/SMC.rds") %>%
+  mutate(chloride_alltime = mean(lab6MC$chloride_mgL, na.rm = TRUE),
+         chloride_summer = mean((lab6MC %>% filter(season == "April - October"))$chloride_mgL, na.rm = TRUE),
+         chloride_winter = mean((lab6MC %>% filter(season == "November - March"))$chloride_mgL, na.rm = TRUE))%>%
   rename(NAME = RIVER_SYS_)
 
-DC.sf <- read_rds("Data/shapefiles/DC.rds") %>%
-  mutate(chloride = mean(labDC$chloride_mgL, na.rm = TRUE))%>%
+DC.sf <- read_rds("Data/shapefiles/DC/DC.rds") %>%
+  mutate(chloride_alltime = mean(labDC$chloride_mgL, na.rm = TRUE),
+         chloride_summer = mean((labDC %>% filter(season == "April - October"))$chloride_mgL, na.rm = TRUE),
+         chloride_winter = mean((labDC %>% filter(season == "November - March"))$chloride_mgL, na.rm = TRUE))%>%
   rename(NAME = RIVER_SYS_)
 
-PBMS.sf <- read_rds("Data/shapefiles/PBMS.rds") %>%
-  mutate(chloride = mean(labPBMS$chloride_mgL, na.rm = TRUE)) %>%
+PBMS.sf <- read_rds("Data/shapefiles/PBMS/PBMS.rds")%>%
+  mutate(chloride_alltime = mean(labPBMS$chloride_mgL, na.rm = TRUE),
+         chloride_summer = mean((labPBMS %>% filter(season == "April - October"))$chloride_mgL, na.rm = TRUE),
+         chloride_winter = mean((labPBMS %>% filter(season == "November - March"))$chloride_mgL, na.rm = TRUE))%>%
   rename(NAME = RIVER_SYS_) %>%
   mutate(NAME = "Pheasant Branch Main Stem")
 
-PBSF.sf <- read_rds("Data/shapefiles/PBSF.rds") %>%
-  mutate(chloride = mean(labPBSF$chloride_mgL, na.rm = TRUE)) %>%
+PBSF.sf <- read_rds("Data/shapefiles/PBSF/PBSF.rds") %>%
+  mutate(chloride_alltime = mean(labPBSF$chloride_mgL, na.rm = TRUE),
+         chloride_summer = mean((labPBSF %>% filter(season == "April - October"))$chloride_mgL, na.rm = TRUE),
+         chloride_winter = mean((labPBSF %>% filter(season == "November - March"))$chloride_mgL, na.rm = TRUE))%>%
   rename(NAME = RIVER_SYS_) %>%
   mutate(NAME = "Pheasant Branch South Fork")
 
-WIC.sf <- read_rds("Data/shapefiles/WIC.rds") %>%
-  mutate(chloride = mean(labWIC$chloride_mgL, na.rm = TRUE)) %>%
+WIC.sf <- read_rds("Data/shapefiles/WIC/WIC.rds")%>%
+  mutate(chloride_alltime = mean(labWIC$chloride_mgL, na.rm = TRUE),
+         chloride_summer = mean((labWIC %>% filter(season == "April - October"))$chloride_mgL, na.rm = TRUE),
+         chloride_winter = mean((labWIC %>% filter(season == "November - March"))$chloride_mgL, na.rm = TRUE))%>%
   rename(NAME = RIVER_SYS_)
 
-SW.sf <- read_rds("Data/shapefiles/SW.rds") %>%
-  mutate(chloride = mean(labSW$chloride_mgL, na.rm = TRUE)) #%>%
+SW.sf <- read_rds("Data/shapefiles/SW/SW.rds") %>%
+  mutate(chloride_alltime = mean(labSW$chloride_mgL, na.rm = TRUE),
+         chloride_summer = mean((labSW %>% filter(season == "April - October"))$chloride_mgL, na.rm = TRUE),
+         chloride_winter = mean((labSW %>% filter(season == "November - March"))$chloride_mgL, na.rm = TRUE))
+#%>%
  # rename(NAME = RIVER_SYS_)
 
-tribs.sf <- bind_rows(YN.sf, YI.sf, YS.sf, SMC.sf, DC.sf, PBMS.sf, PBSF.sf, WIC.sf, SW.sf) 
-lakes.sf <- bind_rows(ME.sf, MO.sf)
+tribs.sf <- bind_rows(YN.sf, YI.sf, YS.sf, SMC.sf, DC.sf, PBMS.sf, PBSF.sf, WIC.sf, SW.sf)  %>%
+  dplyr::select(NAME, chloride_alltime, chloride_summer, chloride_winter, geometry) 
+lakes.sf <- bind_rows(ME.sf, MO.sf) %>%
+  dplyr::select(NAME, chloride_alltime, chloride_summer, chloride_winter, geometry) 
 
 world_gray <- paste0('https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/${z}/${y}/${x}.jpeg')
 
-all.chloride <- as.data.frame(lakes.sf) %>%
-  dplyr::select(NAME, chloride, geometry) 
+# all.chloride <- as.data.frame(lakes.sf) %>%
+#   dplyr::select(NAME, chloride_alltime, chloride_summer, chloride_winter, geometry) 
+# 
+# tribs.sf2 <- as.data.frame(tribs.sf)%>%
+#   dplyr::select(NAME, chloride_alltime, chloride_summer, chloride_winter) #%>%
+#   #distinct()
+# 
+# tribs.geom = data.frame(NAME = c('Yahara River North', 'Sixmile Creek', 'Dorn Creek', 'Pheasant Branch Main Stem', 'Pheasant Branch South Fork', 'Yahara River Isthmus', 'Starkweather Creek', 'Yahara River South', 'Wingra Creek'),lat = c(43.15083333, 43.14683333, 43.145262, 43.10333333, 43.075910, 43.08944444, 43.104292, 43.04718, 43.048883), lon = c(-89.40194444, -89.43694444, -89.475643, -89.51166667, -89.519880, -89.36083333, -89.334455, -89.33605, -89.394132))
+# tribs.geom <- tribs.geom %>%
+#   left_join(tribs, by = "NAME")
 
-tribs <- as.data.frame(tribs.sf)%>%
-  dplyr::select(NAME, chloride) %>%
-  group_by(NAME) %>%
-  summarise(chloride = mean(chloride))
-
-tribs.geom = data.frame(NAME = c('Yahara River North', 'Sixmile Creek', 'Dorn Creek', 'Pheasant Branch Main Stem', 'Pheasant Branch South Fork', 'Yahara River Isthmus', 'Starkweather Creek', 'Yahara River South', 'Wingra Creek'),lat = c(43.15083333, 43.14683333, 43.145262, 43.10333333, 43.075910, 43.08944444, 43.104292, 43.04718, 43.048883), lon = c(-89.40194444, -89.43694444, -89.475643, -89.51166667, -89.519880, -89.36083333, -89.334455, -89.33605, -89.394132))
-tribs.geom <- tribs.geom %>%
-  left_join(tribs, by = "NAME")
-tribs.geom.sf = st_as_sf(tribs.geom, coords = c("lon", "lat"), 
+#need to fix this!!
+tribs.sf = st_as_sf(tribs.geom, 
                       crs = 4326)
 
-all.chloride <- all.chloride %>%
-  rbind(tribs.geom.sf)
+all.chloride <- lakes.sf %>%
+  rbind(tribs.sf)
 
 
-all.chloride <- st_sf(all.chloride)
+all.chloride <- st_sf(all.chloride) %>%
+  arrange(chloride_alltime)
+
+all.chloride$label =  factor(all.chloride$chloride_alltime, levels = unique(all.chloride$chloride_alltime))
+pal = wes_palette("Darjeeling1", 11, "continuous")
 
 ggplot(ME.sf) + 
   annotation_map_tile(type = world_gray, zoom = 12) + # Esri Basemap (zoom sets level of detail, higher = higherRes)
-  geom_sf(data = tribs.sf, aes(color = chloride)) + 
-  geom_sf(data = lakes.sf, aes(color = chloride)) +
-  geom_sf(data = ME.sf, fill = "#5F3673") +
-  geom_sf(data = MO.sf, fill = "#3E246C") +
-  geom_sf_label(data = all.chloride, mapping = aes(label = round(chloride,2))) +
-  scale_color_viridis_c() +
-  #scale_fill_viridis_c() +
+  geom_sf(data = all.chloride, aes(color = label)) + 
+ # geom_sf(data = lakes.sf, aes(color = label)) +
+ #geom_sf(data = ME.sf, aes(fill = chloride_alltime)) +
+  #geom_sf(data = MO.sf, aes(fill = chloride_alltime)) +
+  geom_sf_label(data = all.chloride, mapping = aes(label = round(chloride_alltime,2))) +
+  scale_color_manual(values = wes_palette("Darjeeling1", 11, "continuous")) +
+  #scale_color_viridis_c(option = "inferno") +
+  #scale_fill_viridis_c(option = "inferno") +
   theme_bw() + 
   labs(color = "Average Chloride Concentration (mg/L)", x = "", y = "") +
   annotation_scale(location = "br", width_hint = 0.5,height = unit(0.05,'in')) + # Scale bar
--- figsforpres.R
+++ figsforpres.R
@@ -32,21 +32,21 @@ all_cl2 <- rbind(labPBMS, labPBSF, labSW, labWIC)
 ggplot(all_cl1, aes(ID, chloride_mgL)) +
   geom_boxplot() +
   geom_jitter(aes(color = season)) +
-  scale_color_manual(labels = c("April-October", "November-March"),
-                     values = c("#1C366B", "#F24D29")) +
+  scale_color_manual(labels = c("April-October", "November-March"), values = wes_palette("Darjeeling1")) +
+                     #values = c("#1C366B", "#F24D29")) +
     labs(x = "", y = "Chloride Concentration"~(mg~L^-1)) + L_theme()
 
-ggsave("Plots/figsforpres/low_conc_tribs.png", height = 15, width = 20, units = "cm")
+ggsave("Plots/figsforpres/spatial_distribution/low_conc_tribs.png", height = 15, width = 20, units = "cm")
 
 
 ggplot(all_cl2, aes(ID, chloride_mgL)) +
   geom_boxplot() +
   geom_jitter(aes(color = season)) +
-  scale_color_manual(labels = c("April-October", "November-March"),
-                     values = c("#1C366B", "#F24D29")) +
+  scale_color_manual(labels = c("April-October", "November-March"), values = wes_palette("Darjeeling1")) +
+                     #values = c("#1C366B", "#F24D29")) +
   labs(x = "", y = "Chloride Concentration"~(mg~L^-1)) + L_theme()
 
-ggsave("Plots/figsforpres/high_conc_tribs.png", height = 15, width = 20, units = "cm")
+ggsave("Plots/figsforpres/spatial_distribution/high_conc_tribs.png", height = 15, width = 20, units = "cm")
 
 #low concentration cl timeseries 
 timeseries_lowconc <- rbind(DC_daily_mass %>% mutate(ID = "DC"), SMC_daily_mass %>% mutate(ID = "SMC"), YI_daily_mass %>% mutate(ID = "YI", YN_daily_mass %>% mutate(ID = "YN")))
@@ -54,26 +54,57 @@ timeseries_highconc <- rbind(PBMS_daily_mass %>% mutate(ID = "PBMS"), PBSF_daily
 
 library(wesanderson)
 
-a <- ggplot(timeseries_lowconc, aes(date, daily_concentration_mgL, color = ID)) +
+a <- ggplot(timeseries_lowconc, aes(as.POSIXct(date), daily_concentration_mgL, color = ID)) +
   geom_line() +
-  scale_color_manual(values = wes_palette("BottleRocket2")) +
+  scale_color_manual(values = wes_palette("Darjeeling1")) +
   L_theme() +
-  labs(x = "", y = "Chloride Concentration"~(mg~L^-1))
+  scale_x_datetime(limits = c(datemin, datemax)) +
+  labs(x = "", y = "", title = "Chloride Concentration"~(mg~L^-1)) + theme(legend.position = "bottom")
 
 ggsave("Plots/figsforpres/low_conc_ts.png", height = 15, width = 20, units = "cm")
 
-b <- ggplot(timeseries_highconc, aes(date, daily_concentration_mgL, color = ID)) +
+b <- ggplot(timeseries_highconc %>% filter(ID != "PBMS"), aes(as.POSIXct(date), daily_concentration_mgL, color = ID)) +
   geom_line() +
-  scale_color_manual(values = wes_palette("BottleRocket2")) +
+  scale_color_manual(values = wes_palette("Darjeeling1")) +
+  scale_x_datetime(limits = c(datemin, datemax)) +
   L_theme() +
-  labs(x = "", y = "Chloride Concentration"~(mg~L^-1))
+  labs(x = "", y = "", title = "Chloride Concentration"~(mg~L^-1)) + theme(legend.position = "bottom")
 
+c <- ggplot(timeseries_highconc %>% filter(ID == "PBMS"), aes(as.POSIXct(date), daily_concentration_mgL, color = ID)) +
+  geom_line() +
+  scale_color_manual(values = wes_palette("Darjeeling1")[4]) +
+  scale_x_datetime(limits = c(datemin, datemax)) +
+  L_theme() +
+  labs(x = "", y = "", title = "Chloride Concentration"~(mg~L^-1)) + theme(legend.position = "bottom")
+
+b/c
 ggsave("Plots/figsforpres/high_conc_ts.png", height = 15, width = 20, units = "cm")
 
-
-precip_plot / cl_roads / a
+#plots with precip and salt app.
+precip_plot / cl_roads / a 
 ggsave("Plots/figsforpres/lows.png", height = 15, width = 20, units = "cm")
 
-precip_plot / cl_roads / b
+precip_plot / cl_roads / b / c
 ggsave("Plots/figsforpres/highs.png", height = 15, width = 20, units = "cm")
 
+
+
+#mass into Mendota per season
+timeseries_mass <- rbind(DC_monthly_mass %>% mutate(ID = "DC"), SMC_monthly_mass %>% mutate(ID = "SMC"), YI_monthly_mass %>% mutate(ID = "YI"), YN_monthly_mass %>% mutate(ID = "YN")) %>%
+  rbind(PBMS_monthly_mass %>% mutate(ID = "PBMS"), PBSF_monthly_mass %>% mutate(ID = "PBSF"), SH_monthly_mass %>% mutate(ID = "SH")) %>%
+  mutate(date = as.Date(as.yearmon(year_mon))) %>%
+  mutate(monthly_mass_Mg = ifelse(ID == "YI", monthly_mass_Mg * -1, monthly_mass_Mg))
+
+ggplot(timeseries_mass %>% filter(ID != "PBSF")) +
+  geom_bar(aes(fill = ID, x = date, y = monthly_mass_Mg), position = "stack", stat = "identity") +
+  scale_fill_manual(values = wes_palette("Darjeeling1", n = 6, type = "continuous")) +
+  #scale_fill_viridis_d(option = "inferno", direction = -1) +
+  L_theme() +
+  scale_x_date(date_breaks = "3 months", date_labels = "%b %Y") +
+  scale_y_continuous(n.breaks = 10)
+
+ggsave("Plots/figsforpres/loadingME/massin&outME.png", height = 15, width = 20, units = "cm")
+
+
+
+#maps in AverageChlorideMap.R
