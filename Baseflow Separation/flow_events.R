9aa6884c23cef1dd6d013d45d2d0ea94dbe6059b
LinneaRock <lrock@wisc.edu>
2021-06-21 11:02:48 -0500

    bunch of plots

--- Baseflow Separation/flow_events.R
+++ Baseflow Separation/flow_events.R
@@ -64,22 +64,22 @@ plot_bf_qt(SH_d, "SH")
 
 
 #identify storm flow events based on the following criteria:
-  #The upward slope must be > 1x10^-6 cms
-  #The difference between the peak discharge and the Eckhardt baseflow must be at least half the total average discharge
-  #The downward slope must be < 0 cms and the event is over when the slope changes to >=0 cms
-source("Baseflow Separation/find_peaks_eventduration.R")
- timestamp <- 'date'
- plot_var <- 'runningmeandis'
- sb_pk_thresh <- 0.000001
- sf_pk_thresh <- 0
+#The upward slope must be > 1x10^-6 cms
+#The difference between the peak discharge and the Eckhardt baseflow must be at least half the total average discharge
+#The downward slope must be < 0 cms and the event is over when the slope changes to >=0 cms
+# get function at bottom of this script source("Baseflow Separation/find_peaks_eventduration.R")
+timestamp <- 'date'
+plot_var <- 'runningmeandis'
+sb_pk_thresh <- 0.000001
+sf_pk_thresh <- 0
 
-YN_events_bf <- find.peaks(YN_d, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, YN_cond_data)
-SMC_events_bf <- find.peaks(SMC_d, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, SMC_cond_data)
-DC_events_bf <- find.peaks(DC_d, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, DC_cond_data)
-PBMS_events_bf <- find.peaks(PBMS_d, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, PBMS_cond_data)
-SH_events_bf <- find.peaks(SH_d, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, d.sc.SH %>% dplyr::select(date, sp.cond) %>% rename(runningmean = sp.cond))
-YI_events_bf <- find.peaks(YI_d, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, YI_cond_data)
-PBSF_events_bf <- find.peaks(PBSF_d, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, PBSF_cond_data)
+YN_events_bf <- find..peaks(YN_d, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, YN_cond_data)
+SMC_events_bf <- find..peaks(SMC_d, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, SMC_cond_data)
+DC_events_bf <- find..peaks(DC_d, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, DC_cond_data)
+PBMS_events_bf <- find..peaks(PBMS_d, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, PBMS_cond_data)
+SH_events_bf <- find..peaks(SH_d, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, d.sc.SH %>% dplyr::select(date, sp.cond) %>% rename(runningmean = sp.cond))
+YI_events_bf <- find..peaks(YI_d, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, YI_cond_data)
+PBSF_events_bf <- find..peaks(PBSF_d, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, PBSF_cond_data)
 
 #plots
 source("Baseflow Separation/cq_analysis_plots_fct.R")
@@ -105,11 +105,11 @@ options(scipen = 999)
 
 (a | b | c | d) / (e | f | g) +
   plot_annotation(
-                  caption = "Figure X. Concentration-discharge plots on log-log axes for each study site. Data includes all
+    caption = "Figure X. Concentration-discharge plots on log-log axes for each study site. Data includes all
 specific conductivity (SC) and discharge over the entire study period. Corresponding statistics
 are in Table X.",
-                  theme = theme(plot.tag = element_text(size = 10), 
-                                plot.caption = element_text(size = 10, hjust = 0)))
+    theme = theme(plot.tag = element_text(size = 10), 
+                  plot.caption = element_text(size = 10, hjust = 0)))
 
 ggsave("Plots/QC_plots/Eckhardt_Method/all_cq/figure.png", height = 4.25, width = 6.25, units = "in")
 
@@ -126,13 +126,13 @@ library(gt)
 library(webshot)
 all_cq_stats <- data.frame(
   River = c(
-   "YN",
-   "YI",
-   "SMC",
-   "DC",
-   "PBMS",
-   "SH",
-   "PBSF"
+    "YN",
+    "YI",
+    "SMC",
+    "DC",
+    "PBMS",
+    "SH",
+    "PBSF"
   ),
   Slope = c(
     slope_cq(fitYN),
@@ -160,8 +160,8 @@ all_cq_stats <- data.frame(
   #   r.sqr.lm_cq(fitPBMS),
   #   r.sqr.lm_cq(fitSH),
   #   r.sqr.lm_cq(fitPBSF)
-    
- # ),
+  
+  # ),
   P_value = c("<0.0001", "<0.0001", "<0.0001", "<0.0001", "<0.0001", "<0.0001", "<0.0001"
               # pvalue(labYN, fieldcondYN, YN_cond_data),
               # pvalue(lab6MC, fieldcond6MC, SMC_cond_data),
@@ -327,10 +327,10 @@ all_events$season = factor(all_events$season, levels = c("Oct-Dec", "Jan-Mar", "
 ggplot() +
   labs(x = "SC-Q Slope", y = "Tributary") + 
   geom_point(all_events, mapping = aes(slope, trib, color = season), size = 2.5, shape = 21) +
-   scale_color_manual(labels = c("Oct-Dec", "Jan-Mar", "Apr-Jun", "Jul-Sep"),
-                      values = c("#1DACE8", "#1C366B", "#F24D29", "#E5C4A1")) +
-   # scale_fill_manual(labels = c("Oct-Dec", "Jan-Mar", "Apr-Jun", "Jul-Sep"),
-   #                    values = c("#1DACE8", "#1C366B", "#F24D29", "#E5C4A1")) +
+  scale_color_manual(labels = c("Oct-Dec", "Jan-Mar", "Apr-Jun", "Jul-Sep"),
+                     values = c("#1DACE8", "#1C366B", "#F24D29", "#E5C4A1")) +
+  # scale_fill_manual(labels = c("Oct-Dec", "Jan-Mar", "Apr-Jun", "Jul-Sep"),
+  #                    values = c("#1DACE8", "#1C366B", "#F24D29", "#E5C4A1")) +
   theme(legend.title = element_blank()) +
   L_theme() +
   annotate("rect", xmin = -0.05, xmax = 0.05, ymin = 0, ymax = Inf, alpha = 0.2, color = "grey") +
@@ -343,4 +343,181 @@ for SC and flow data during baseflow only. Grey box indicates chemostatic behavi
 
 ggsave("Plots/QC_plots/Eckhardt_Method/event_slopes.png", height = 4.25, width = 6.25, units = "in")
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#df is a dataframe with baseflow and threshold calculated already 
+# timestamp <- 'date'
+# plot_var <- 'runningmeandis'
+# sb_pk_thresh <- 0.000001
+# sf_pk_thresh <- 0
+#cond_data is the EC timeseries for the site or cl_ts_data is the chloride timeseries calculated from the cond
+
+#the find.peaks function should ingest a dataframe and output a dataframe with the peaks idenitified
+find..peaks <- function(df.orig, timestamp, plot_var, sb_pk_thresh, sf_pk_thresh, cond_data){
+  df <- df.orig %>%
+    group_by(as.Date(date, format = "%m/%d/%y")) %>%
+    summarise(mean(runningmeandis), mean(value), mean(threshold_peak)) %>%
+    rename(date = 'as.Date(date, format = "%m/%d/%y")',
+           runningmeandis = 'mean(runningmeandis)',
+           value = 'mean(value)',
+           threshold_peak = 'mean(threshold_peak)') %>%
+    ungroup() %>%
+    as.data.frame() %>%
+    mutate(threshold_peak = value + (mean(runningmeandis))/2)
+  
+  
+  #convert to seconds
+  #df$Date <- as.POSIXct(df$Date)
+  df$Seconds <- as.numeric(unlist(df[,timestamp]))
+  ##-remove NAs
+  #df <- df[!is.na(df[,plot_var]),]
+  ##-make sure plotting variable is greater than zero
+  #df <- df[df[,plot_var] > 0,]
+  ##-create an empty column for the slope forward and backward
+  df$slp.b <- rep(NA, length.out = nrow(df))
+  df$slp.f <- rep(NA, length.out = nrow(df))
+  
+  for(i in 2:(nrow(df)-1)){
+    ##-calculate the slope back one timestep
+    df$slp.b[i] <- (df[,plot_var][i]-df[,plot_var][(i-1)])/(df$Seconds[i]-df$Seconds[(i-1)])
+    ##-calculate the slope forward one timestep
+    df$slp.f[i] <- (df[,plot_var][(i+1)]-df[,plot_var][i])/(df$Seconds[(i+1)]-df$Seconds[i])
+  }
+  
+  ##-make a column for the peak of each event flagged by a change in derivative
+  df$peak.flag <- rep(NA, length.out = nrow(df))
+  
+  ##-now flag those derivative changes
+  for(i in 2:(nrow(df)-1)){
+    ##-if the slope back is greater than some threshold and the slope forward is negative, flag it as a peak
+    ##-default sb_pk_thresh 0.000001
+    ##-default sf_pk_thresh 0
+    if(df$slp.b[i]>sb_pk_thresh & df$slp.f[i]<sf_pk_thresh){
+      df$peak.flag[i] <- 1
+    }else{
+      ##-otherwise don't
+      df$peak.flag[i] <- 0
+    }
+  }
+  
+  ggplot() +
+    geom_line(df, mapping= aes(date, runningmeandis)) +
+    geom_point(df%>%filter(peak.flag == 1),mapping = aes(date, runningmeandis), color = "red") +
+    geom_line(df, mapping = aes(date, threshold_peak),color = "blue")
+  
+  df <- df %>%
+    mutate(peak.flag = ifelse(peak.flag == 1 & runningmeandis > threshold_peak, 1, 0)) 
+  
+  
+  
+  ##-Flag the changes in derivatitives, events is the row of single site which have events
+  events <- which(df$peak.flag == 1)
+  ##-if there are no events return df
+  if(length(events) == 0){
+    return(df)
+  }else{
+    ##-within single site, make a column that will signal which observations belong to which peak
+    ##-this will turn into the rising and falling limbs
+    df$event.flag <- rep(NA, length.out = nrow(df))
+    
+    for(i in 1:length(events)){
+      k <- events[i]
+      ##-the while loop goes forward in time until the slope forward is no longer negative
+      while(df$slp.f[k] <0){
+        ##-and labels that with the event number (i) and makes it positive (+)
+        df$event.flag[k] <- i
+        k <- k +1
+        
+        ##-if the last row of single sites is an event peak then move on
+        if(k == nrow(df)){
+          break
+        }else{
+        }
+      }
+      
+      ##-now step backward in time for the falling limb
+      ##-starting with the point directly before the peak
+      j <- events[i]-1
+      ##-if it's the first two data points in the site don't bother
+      if(j == 1|j == 0){
+        next
+      }else{
+        ##-as you step backwards label the days with the event number (i) and make it negative (-)
+        ##-this time to indicate it is the rising limb (before the peak)
+        while(df$slp.b[j] > 0){
+          df$event.flag[j] <- -1*i
+          ##-label j-1 as part of the rising limb too because j-1 won't be grouped with the rising limb
+          ##-if it has a negative slope on the next step going back
+          df$event.flag[j-1] <- -1*i
+          j <- j - 1
+          ##-if i is 1,2 or 3 in the data frame forget about it
+          if(j == 2| j == 1| j == 0){
+            break
+          }else{
+          }
+        }
+      }
+    }
+  }
+  
+  df1 <- df.orig %>%
+    rename(DATE = date) %>%
+    mutate(date = as.Date(DATE)) %>%
+    left_join(df, by = "date") %>%
+    rename(dummy = date) %>%
+    rename(date = DATE)
+  
+  
+  df_combine <- df1 %>%
+    left_join(cond_data)
+  
+  df_peaks <- df_combine %>%
+    drop_na(event.flag) %>%
+    rename(event_flow = runningmeandis.x,
+           event_cond = runningmean)
+  
+  df_baseflow_only <- df_combine %>%
+    filter(is.na(event.flag)) %>%
+    rename(bf = runningmeandis.x,
+           bf_cond = runningmean)
+  
+  df_all_flow <- df_baseflow_only %>%
+    bind_rows(df_peaks) %>%
+    arrange(date) %>%
+    dplyr::select(date, bf, bf_cond, event_flow, event_cond, value.x, threshold_peak.y, peak.flag, event.flag) %>%
+    rename(value = value.x,
+           threshold_peak = threshold_peak.y)
+  return(df_all_flow)
+}
 
